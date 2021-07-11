584284db42d7c3ebcc393dd881f4a72d1d7ab135
AbdulrehmanSuliman <55790261+AbdulrehmanSuliman@users.noreply.github.com>
2021-07-12 01:16:20 +0200

    excel V1

--- Front-end/frontend/src/Components/Admin/DataTable.js
+++ Front-end/frontend/src/Components/Admin/DataTable.js
@@ -5,10 +5,8 @@ function DataTable(props){
     return(
         <div className="modal__backdrop" >
             <div className="modal__container__Result">
-                <h1 className="modal__title">نتيجة التنسيق</h1>
-                <p id="para">
-                النتيجة
-                </p>
+                <h1 className="modal__title">بيانات الطلاب</h1>
+                
                 <button type="button" onClick={ () =>{
                     props.onClose();}}>
                     غلق
