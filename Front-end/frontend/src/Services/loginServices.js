cc9fa5a7183c5ed6ef15f251dddca2de8ba9301a
mennat allah nawar <55787939+mennatallah-nawar@users.noreply.github.com>
2021-07-25 14:16:36 +0200

    login request

---
+++ Front-end/frontend/src/Services/loginServices.js
@@ -0,0 +1,37 @@
+import axios from "axios"
+import configData from '../config/production.json'
+const SERVER_URL = configData.SERVER_URL ;
+import { useHistory } from 'react-router-dom';
+const history = useHistory();
+
+export default async function login (data){
+    try{
+        const response = await axios.post( SERVER_URL+'/endpoint',{data});
+        //Success
+        localStorage.setItem('access token', response.data.tokens.access);
+        history.push('/user');
+        return(response)
+
+    } catch (error){
+        if (error.response){
+          /*
+          * The request was made and the server responded with a
+          * status code that falls out of the range of 2xx
+          */
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request){
+          /*
+          * The request was made but no response was received, `error.request`
+          * is an instance of XMLHttpRequest in the browser and an instance
+          * of http.ClientRequest in Node.js
+          */
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request and triggered an Error
+          console.log('Error', error.message);
+        }
+        console.log(error);
+    }
+};
