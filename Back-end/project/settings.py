8fcdce1c0dd5cd56bc7e112e352a426db0953478
Ziyad <17732483+ziyadss@users.noreply.github.com>
2021-07-26 19:06:12 +0200

    automated to deploy on fotone.me, still needs secrets to work

--- Back-end/project/settings.py
+++ Back-end/project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['mzayed.net', 'www.mzayed.net', '127.0.0.1']
+ALLOWED_HOSTS = ['fotone.me','www.fotone.me']
 
 AUTH_USER_MODEL="users.User"
 
