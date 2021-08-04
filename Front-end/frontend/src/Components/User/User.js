c615fdb920cdc37636bfbebcfba87809a97033ff
samarnabil <samarnabil22@gmail.com>
2021-08-04 22:09:23 +0200

    notification popup

--- Front-end/frontend/src/Components/User/User.js
+++ Front-end/frontend/src/Components/User/User.js
@@ -7,14 +7,17 @@ import ResultModal from "./ResultModal";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChartBar, faGraduationCap } from '@fortawesome/free-solid-svg-icons'
 import { putUserChoices } from "../../Services/userServices";
-import Notification from "../../Constants/Notification";
+import PopUp from "../../Constants/PopUp";
 
 function User() {
     const tansiqIcon = <FontAwesomeIcon icon={faChartBar} color="#f5ba13"/>
     const resultIcon = <FontAwesomeIcon icon={faGraduationCap} color="#f5ba13"/>
-    const [isTansiqOpen, setIsTansiqOpen] = useState(false)
-    const [isResultOpen, setIsResultOpen] = useState(false)    
-
+    const [isTansiqOpen, setIsTansiqOpen] = useState(false);
+    const [isResultOpen, setIsResultOpen] = useState(false);   
+    const [isSucces , setIsSuccess] = useState(false);
+    const [isError , setIsError] = useState(false);
+    const [isInfo , setIsInfo] = useState(false)
+ 
     function toggleTansiqModal(){
         setIsTansiqOpen(!isTansiqOpen)
     }
@@ -25,13 +28,26 @@ function User() {
 
     function confirmChange(order){
         setIsTansiqOpen(false)
+        setIsInfo(true);
         const orderToString = order.toString()
         console.log(orderToString);
         // put request
-        // (async () => {
-        //     const response = await putUserChoices(orderToString);
-        //     console.log(response)
-        //   })(); 
+        (async () => {
+            const response = await putUserChoices(orderToString);
+            console.log(response)
+            if (response.status == 200){
+                handlePopUp ()
+                console.log(response.status)
+                setIsSuccess(true);
+            }
+          })(); 
+          console.log(isSucces)
+    }
+
+    function handlePopUp (){
+        setIsSuccess(false);
+        setIsError(false);
+        setIsInfo(false);
     }
 
     return( 
@@ -52,7 +68,8 @@ function User() {
 
                 </div>
             </div>
-            {/* <Notification type="info"/>    */}
+            {isSucces && <PopUp type="success" title="نجحت العملية" message="تم حفظ التغيرات" onEnd={handlePopUp} interval={7000}/>}
+            {isInfo && <PopUp type="info" title=" برجاء الانتظار" message=" جاري تنفيذ التغيرات " onEnd={handlePopUp} interval={4000}/>}
             {isTansiqOpen && <TansiqModal onConfirm={confirmChange} onClose={toggleTansiqModal}/>}
             {isResultOpen && <ResultModal onClose={toggleResultModal}/>}
         </>
